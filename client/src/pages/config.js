c6b4d36377cc698322ca2c9d34bcc5e4d20a919d
Mr-Yogesh01 <yvanjari851@gmail.com>
2025-11-06 00:44:02 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://backend.yoghesh.xyz";
+const API_BASE_URL = "http://backend.yoghesh.xyz:86";
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
